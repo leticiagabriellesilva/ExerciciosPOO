420e73bb7f770ed4ed9495392c7526298a659063
Leticia Silva <128447579+leticiagabriellesilva@users.noreply.github.com>
2024-08-12 22:02:38 -0300

    Update Professor.java

--- src/Professor.java
+++ src/Professor.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Professor {
+package PACKAGE_NAME;
+public class Professor extends Funcionario {
+    private String titulacao;
+
+    public Professor(String login, String senha, int numFuncional, String titulacao) {
+        super(login, senha, numFuncional);
+        this.titulacao = titulacao;
+    }
+
+    public void digitarNotas(int turma) {
+        System.out.println("Notas digitadas para a turma " + turma);
+    }
 }
