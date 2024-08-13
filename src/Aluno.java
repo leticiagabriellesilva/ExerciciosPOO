06c2819db7f0c5489ea93026d3e0ce03da4915bb
Leticia Silva <128447579+leticiagabriellesilva@users.noreply.github.com>
2024-08-12 22:00:57 -0300

    Update Aluno.java

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Aluno {
+package PACKAGE_NAME;
+public class Aluno extends Usuario {
+    private int matricula;
+    private String curso;
+
+    public Aluno(String login, String senha, int matricula, String curso) {
+        super(login, senha);
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public void matricularSe() {
+        System.out.println("Aluno matriculado no curso: " + this.curso);
+    }
+
+    public void trancarMatricula() {
+        System.out.println("Matrícula trancada para o curso: " + this.curso);
+    }
 }
+
