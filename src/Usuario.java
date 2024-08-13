6d466fce5770c747440dced7560dbe0358a3eef7
Leticia Silva <128447579+leticiagabriellesilva@users.noreply.github.com>
2024-08-12 22:01:34 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Usuario {
+package PACKAGE_NAME;
+public class Usuario {private String login;
+    private String senha;
+
+    public Usuario(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public boolean logar(String login, String senha) {
+        return this.login.equals(login) && this.senha.equals(senha);
+    }
 }
