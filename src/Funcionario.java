84058009def921d801b52c3ac9291fa1f1e978c6
Leticia Silva <128447579+leticiagabriellesilva@users.noreply.github.com>
2024-08-12 22:02:14 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Funcionario {
-}
+package PACKAGE_NAME;
+import java.time.LocalTime;
+
+public class Funcionario extends Usuario {
+    private int numFuncional;
+
+    public Funcionario(String login, String senha, int numFuncional) {
+        super(login, senha);
+        this.numFuncional = numFuncional;
+    }
+
+    public void registrarHorario(String hora) {
+        LocalTime horario = LocalTime.parse(hora);
+        System.out.println("Horário de trabalho registrado às: " + horario);
+    }
