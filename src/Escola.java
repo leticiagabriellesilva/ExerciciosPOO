6e2868a5f20156608f7c7e71080cc52399854cda
Leticia Silva <128447579+leticiagabriellesilva@users.noreply.github.com>
2024-08-12 22:03:06 -0300

    Update Escola.java

--- src/Escola.java
+++ src/Escola.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Escola {
+package PACKAGE_NAME;
+public class Escola {
+  public static void main(String[] args) {
+        // aluno
+        Aluno aluno = new Aluno("alunoLogin", "alunoSenha", 12345, "Analise e Desenvolvimento de Sistemas");
+        aluno.matricularSe();
+
+        // funcionário
+        Funcionario funcionario = new Funcionario("funcLogin", "funcSenha", 67890);
+        funcionario.registrarHorario("22:00");
+
+        // professor
+        Professor professor = new Professor("profLogin", "profSenha", 54321, "Doutor em Computação");
+        professor.digitarNotas(6);
+    }
 }
